369bed678eca443c8e4cb7feea034b35d22bfcc9
Neha Maria Joji <145547103+Nehamariaj@users.noreply.github.com>
2024-04-10 16:27:13 +0530

    Intro.py
    Introduction to some basic functions in python

---
+++ Intro.py
@@ -0,0 +1,22 @@
+print('*******To check Type of a variable********')
+print(type(98.9))
+print(type('Hai'))
+print(type(10))
+
+print('************Type conversion*************')
+print(float(9)+1) #o/p=10.0
+i=42
+print(float(i)) #o/p=42.0
+print(9/2) #integer division produces a floating point number by default
+print(2.0/10.0)
+
+print('********STRING CONVERSIONS**********')
+a='123'
+print(type(a))
+b=int(a)
+print(type(b))
+print(b+1)
+
+print('******Method to get User input********')
+name=input('What is your name?')
+print('Welcome '+name)
